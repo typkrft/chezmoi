ac3ab83db346ab4d02fdd4436b895ba8df9b103d
typkrft <>
2022-11-07 14:26:45 -0500

    Updated .chezmoi.toml.tmpl

--- run_before_00-set-env-vars.sh
+++ run_before_00-set-env-vars.sh
@@ -3,6 +3,7 @@
 set -eu 
 
 # Set IP Env 
-IP=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | tail -n1)
-export "$IP"
+ip_addr=$(ifconfig | grep "inet " | grep -Fv 127.0.0.1 | awk '{print $2}' | tail -n1)
+export IP="$ip_addr"
+
 # Set GUI
