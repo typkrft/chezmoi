a52751a1abc6cf88bab8383d80f318c606577e5c
typkrft <>
2022-11-16 04:27:41 -0500

    Improved /docker/context function

--- dot_config/zsh/plugins/24-docker.zsh
+++ dot_config/zsh/plugins/24-docker.zsh
@@ -3,9 +3,14 @@
 if command -v docker &>/dev/null; then
     docker_context() {
         contexts=($(sudo docker context list | awk 'NR>1 {print $1}'))
-        for context in ${contexts[@]}; do 
+        current_context=$(sudo docker context ls | grep '*' | awk '{print $1}')
+        for context in ${contexts[@]}; do
             if [[ $(pwd) == *"/docker/$context"* ]]; then
-                sudo docker context use $context 
+                if [[ $context = $current_context ]]; then
+                    return 0
+                fi
+                sudo docker context use $context &>/dev/null
+                echo "Now Using Docker Context: $context"
                 return 0
             fi
         done
