447b7b032ffd6f9f1898e4c78ac5792376558ca7
typkrft <>
2022-11-16 20:35:56 -0500

    Updated docker chpwd function

--- dot_config/zsh/plugins/24-docker.zsh
+++ dot_config/zsh/plugins/24-docker.zsh
@@ -2,18 +2,20 @@
 
 if command -v docker &>/dev/null; then
     docker_context() {
-        contexts=($(sudo docker context list | awk 'NR>1 {print $1}'))
-        current_context=$(sudo docker context ls | grep '*' | awk '{print $1}')
-        for context in ${contexts[@]}; do
-            if [[ $(pwd) == *"/docker/$context"* ]]; then
-                if [[ $context = $current_context ]]; then
+        if [[ $(pwd) == *"/docker/"* ]]; then
+            contexts=($(sudo docker context list | awk 'NR>1 {print $1}'))
+            current_context=$(sudo docker context ls | grep '*' | awk '{print $1}')
+            for context in ${contexts[@]}; do
+                if [[ $(pwd) == *"/docker/$context"* ]]; then
+                    if [[ $context = $current_context ]]; then
+                        return 0
+                    fi
+                    sudo docker context use $context &>/dev/null
+                    echo "Now Using Docker Context: $context"
                     return 0
                 fi
-                sudo docker context use $context &>/dev/null
-                echo "Now Using Docker Context: $context"
-                return 0
-            fi
-        done
+            done
+        fi
     }
     chpwd_functions=(docker_context $chpwd_functions)
 fi
